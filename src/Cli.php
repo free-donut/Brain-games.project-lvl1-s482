79f29a90995d4792ac4346c3e0b55c436a5f81c3
free-donut <yula@freedonut.space>
2019-04-25 21:59:01 +0300

    fix errors src/Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -6,7 +6,7 @@ use function \cli\line;
 
 function run()
 {
-	line('Welcome to the Brain Game!');
-	$name = \cli\prompt('May I have your name?');
-	line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }

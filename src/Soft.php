4b1a7e2244b71a291aea9c708f991f4af94248e4
free-donut <yula@freedonut.space>
2019-04-28 15:02:10 +0300

    edit src/Soft.php

---
+++ src/Soft.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace BrainGames\Soft;
+
+use function \cli\line;
+
+//приветсивие, узнать имя
+function run($rules)
+{
+	//приветствие
+	line('Welcome to the Brain Game!');
+	//правила
+    line($rules);
+    //узнать имя
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    /*
+    questionEven($name);
+    */
+    
+}
+
+/*
+//вопросы
+function questions($name, $question, $correectAnswer)
+{
+    for ($correctAnswerCount = 0; $correctAnswerCount < 3; $correctAnswerCount++) {
+        line('Question: %s', $question);
+        $answer = \cli\prompt('Your answer');
+        if ($answer == $correectAnswer) {
+            line('Correct!');
+        } else {
+            line('\'%s\' is wrong answer ;(.', $answer);
+            line('Correct answer was \'%s\'', $correectAnswer);
+            line("Let's try again, %s", $name);
+            break;
+        }
+    }
+    if ($correctAnswerCount == 3) {
+        line('Congratulations, %s', $name);
+    }
+}
+
+//вопрос
+function question($func)
+{
+	return $func;
+
+}
+
+//правильный ответ
+function correectAnswer($question, $func)
+{
+	return $func($question);
+
+}
+*/

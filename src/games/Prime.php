0c3c94568fa60735dde507741d8fddca28ce31a6
free-donut <yula@freedonut.space>
2019-05-15 21:43:54 +0300

    make new game brain primefix

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -7,19 +7,19 @@ const RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function ifPrime($number, $x = 2)
 {
-    if($x > $number/2){
-      return true;
-    } if($number % $x === 0) {
+    if ($x > $number / 2) {
+        return true;
+    } if ($number % $x === 0) {
         return false;
     } else {
-        return ifPrime($number, $x +1);
+        return ifPrime($number, $x + 1);
     }
 }
 
 function runGame()
 {
     $getData = function () {
-        $number =rand(2, 4000);
+        $number = rand(2, 4000);
         $question = $number;
         if (ifPrime($number)) {
             $correctAnswer = 'yes';
